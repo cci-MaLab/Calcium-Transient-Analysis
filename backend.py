e924938860e5ce0af00f076f0df2a608e7180317
Haoying Fu <rainbow_pet@163.com>
2023-08-20 16:37:21 -0400

    update

--- backend.py
+++ backend.py
@@ -1,6 +1,83 @@
 # This is where all the ultility functions are stored
 
 # The first function is to load in the data
+import dask as da
+import dask.array as darr
+import numpy as np
+import pandas as pd
+import xarray as xr
+from os.path import isdir, isfile
+from os.path import join as pjoin
+from os import listdir
+from typing import Callable, List, Optional, Union
+
+def open_minian(
+    dpath: str, post_process: Optional[Callable] = None, return_dict=False
+) -> Union[dict, xr.Dataset]:
+    """
+    Load an existing minian dataset.
+
+    If `dpath` is a file, then it is assumed that the full dataset is saved as a
+    single file, and this function will directly call
+    :func:`xarray.open_dataset` on `dpath`. Otherwise if `dpath` is a directory,
+    then it is assumed that the dataset is saved as a directory of `zarr`
+    arrays, as produced by :func:`save_minian`. This function will then iterate
+    through all the directories under input `dpath` and load them as
+    `xr.DataArray` with `zarr` backend, so it is important that the user make
+    sure every directory under `dpath` can be load this way. The loaded arrays
+    will be combined as either a `xr.Dataset` or a `dict`. Optionally a
+    user-supplied custom function can be used to post process the resulting
+    `xr.Dataset`.
+
+    Parameters
+    ----------
+    dpath : str
+        The path to the minian dataset that should be loaded.
+    post_process : Callable, optional
+        User-supplied function to post process the dataset. Only used if
+        `return_dict` is `False`. Two arguments will be passed to the function:
+        the resulting dataset `ds` and the data path `dpath`. In other words the
+        function should have signature `f(ds: xr.Dataset, dpath: str) ->
+        xr.Dataset`. By default `None`.
+    return_dict : bool, optional
+        Whether to combine the DataArray as dictionary, where the `.name`
+        attribute will be used as key. Otherwise the DataArray will be combined
+        using `xr.merge(..., compat="no_conflicts")`, which will implicitly
+        align the DataArray over all dimensions, so it is important to make sure
+        the coordinates are compatible and will not result in creation of large
+        NaN-padded results. Only used if `dpath` is a directory, otherwise a
+        `xr.Dataset` is always returned. By default `False`.
+
+    Returns
+    -------
+    ds : Union[dict, xr.Dataset]
+        The resulting dataset. If `return_dict` is `True` it will be a `dict`,
+        otherwise a `xr.Dataset`.
+
+    See Also
+    -------
+    xarray.open_zarr : for how each directory will be loaded as `xr.DataArray`
+    xarray.merge : for how the `xr.DataArray` will be merged as `xr.Dataset`
+    """
+    if isfile(dpath):
+        ds = xr.open_dataset(dpath).chunk()
+    elif isdir(dpath):
+        dslist = []
+        for d in listdir(dpath):
+            arr_path = pjoin(dpath, d)
+            if isdir(arr_path):
+                arr = list(xr.open_zarr(arr_path).values())[0]
+                arr.data = darr.from_zarr(
+                    os.path.join(arr_path, arr.name), inline_array=True
+                )
+                dslist.append(arr)
+        if return_dict:
+            ds = {d.name: d for d in dslist}
+        else:
+            ds = xr.merge(dslist, compat="no_conflicts")
+    if (not return_dict) and post_process:
+        ds = post_process(ds, dpath)
+    return ds
 
 def load_data(path, **kwargs):
     # Load the data in as necessary
