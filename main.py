fd2b7b83d52441877f086ba560bcc90c29830592
GrowlingM1ke <michallange1995@gmail.com>
2023-08-20 15:21:26 -0400

    Added basic functionality to select data folder

--- main.py
+++ main.py
@@ -1,80 +1,6 @@
-from PyQt6.QtGui import *
-from PyQt6.QtWidgets import *
-from PyQt6.QtCore import *
-
-import time
-import traceback, sys
-
-
-class WorkerSignals(QObject):
-    '''
-    Defines the signals available from a running worker thread.
-
-    Supported signals are:
-
-    finished
-        No data
-
-    error
-        tuple (exctype, value, traceback.format_exc() )
-
-    result
-        object data returned from processing, anything
-
-    progress
-        int indicating % progress
-
-    '''
-    finished = pyqtSignal()
-    error = pyqtSignal(tuple)
-    result = pyqtSignal(object)
-    progress = pyqtSignal(int)
-
-
-class Worker(QRunnable):
-    '''
-    Worker thread
-
-    Inherits from QRunnable to handler worker thread setup, signals and wrap-up.
-
-    :param callback: The function callback to run on this worker thread. Supplied args and
-                     kwargs will be passed through to the runner.
-    :type callback: function
-    :param args: Arguments to pass to the callback function
-    :param kwargs: Keywords to pass to the callback function
-
-    '''
-
-    def __init__(self, fn, *args, **kwargs):
-        super(Worker, self).__init__()
-
-        # Store constructor arguments (re-used for processing)
-        self.fn = fn
-        self.args = args
-        self.kwargs = kwargs
-        self.signals = WorkerSignals()
-
-        # Add the callback to our kwargs
-        self.kwargs['progress_callback'] = self.signals.progress
-
-    @pyqtSlot()
-    def run(self):
-        '''
-        Initialise the runner function with passed args, kwargs.
-        '''
-
-        # Retrieve args/kwargs here; and fire processing using them
-        try:
-            result = self.fn(*self.args, **self.kwargs)
-        except:
-            traceback.print_exc()
-            exctype, value = sys.exc_info()[:2]
-            self.signals.error.emit((exctype, value, traceback.format_exc()))
-        else:
-            self.signals.result.emit(result)  # Return the result of the processing
-        finally:
-            self.signals.finished.emit()  # Done
-
+from PyQt6.QtWidgets import (QApplication, QMainWindow, QStyle, QFileDialog, QMessageBox)
+from PyQt6.QtGui import (QAction)
+from threads import Worker
 
 
 class MainWindow(QMainWindow):
@@ -82,63 +8,62 @@ class MainWindow(QMainWindow):
 
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
+        self.setWindowTitle("Cell CLustering Tool")
+        self.setMinimumSize(600, 400)
 
-        self.counter = 0
+        # Data stuff
+        self.data = []
 
-        layout = QVBoxLayout()
-
-        self.l = QLabel("Start")
-        b = QPushButton("DANGER!")
-        b.pressed.connect(self.oh_no)
-
-        layout.addWidget(self.l)
-        layout.addWidget(b)
-
-        w = QWidget()
-        w.setLayout(layout)
-
-        self.setCentralWidget(w)
+        # Menu Bar
+        pixmapi = QStyle.StandardPixmap.SP_DirIcon
+        button_action = QAction(self.style().standardIcon(pixmapi), "&Load Data", self)
+        button_action.setStatusTip("Select a Folder to load in data")
+        button_action.triggered.connect(self.onMyToolBarButtonClick)
+        menu = self.menuBar()
+        file_menu = menu.addMenu("&File")
+        file_menu.addAction(button_action)
 
         self.show()
 
-        self.threadpool = QThreadPool()
-        print("Multithreading with maximum %d threads" % self.threadpool.maxThreadCount())
+    def printError(self, s):
+        dlg = QMessageBox(self)
+        dlg.setWindowTitle("I have a question!")
+        if isinstance(s, tuple):
+            text = f"For path {s[1]} the following error occurred:\n {s[0]}"
+        else:
+            text = s
+        dlg.setText(text)
+        dlg.setIcon(QMessageBox.Icon.Critical)
+        dlg.exec()
 
-        self.timer = QTimer()
-        self.timer.setInterval(1000)
-        self.timer.timeout.connect(self.recurring_timer)
-        self.timer.start()
-
-    def progress_fn(self, n):
-        print("%d%% done" % n)
-
-    def execute_this_fn(self, progress_callback):
-        for n in range(0, 5):
-            time.sleep(1)
-            progress_callback.emit(n*100/4)
-
-        return "Done."
-
-    def print_output(self, s):
-        print(s)
-
-    def thread_complete(self):
+    def threadComplete(self):
         print("THREAD COMPLETE!")
 
-    def oh_no(self):
-        # Pass the function to execute
-        worker = Worker(self.execute_this_fn) # Any other args, kwargs are passed to the run function
+    def addData(self, data):
+        self.data.append(data)
+
+
+    def onMyToolBarButtonClick(self, s):
+        fname = QFileDialog.getExistingDirectory(
+            self,
+            "Open File",
+        )
+        
+        # Over here Haoying you should call your load_data function from backend.py using the Worker class
+        # Something like this:
+        '''
+        worker = Worker(load_data, fname)
+        self.threadpool.start(worker)
+        worker.signals.error.connect(self.printError)
         worker.signals.result.connect(self.print_output)
-        worker.signals.finished.connect(self.thread_complete)
-        worker.signals.progress.connect(self.progress_fn)
+        worker.signals.finished.connect(self.threadComplete)
 
         # Execute
         self.threadpool.start(worker)
+        '''
+        
 
 
-    def recurring_timer(self):
-        self.counter +=1
-        self.l.setText("Counter: %d" % self.counter)
 
 
 app = QApplication([])
