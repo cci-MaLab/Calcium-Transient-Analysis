315f406900f2e7f381ff3ee033f469c6a81ca367
Haoying Fu <rainbow_pet@163.com>
2023-08-24 00:09:31 -0400

    merge

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from PyQt5.QtWidgets import (QApplication, QMainWindow, QStyle, QFileDialog, QMe
                             QLabel, QVBoxLayout, QHBoxLayout, QGridLayout, QComboBox, QWidget,
                             QFrame)
 from PyQt5.QtCore import (QThreadPool)
-from custom_widgets import LoadingDialog, ParamDialog
+from custom_widgets import LoadingDialog, ParamDialog, VisualizeClusterWidget
 from PyQt5 import Qt
 import sys
 import os
+import json
 sys.path.insert(0, ".")
 from backend import SessionFeature
 
@@ -21,17 +22,22 @@ class MainWindow(QMainWindow):
         self.threadpool = QThreadPool()
 
         # Data stuff
-        self.data = []
+        self.sessions = []
         self.path_list = {}
 
         # Menu Bar
-        pixmapi = QStyle.StandardPixmap.SP_DirIcon
-        button_action = QAction(self.style().standardIcon(pixmapi), "&Load Data", self)
-        button_action.setStatusTip("Select a Folder to load in data")
-        button_action.triggered.connect(self.onMyToolBarButtonClick)
+        pixmapi_folder = QStyle.StandardPixmap.SP_DirIcon
+        button_folder = QAction(self.style().standardIcon(pixmapi_folder), "&Load Data", self)
+        button_folder.setStatusTip("Select a Folder to load in data")
+        button_folder.triggered.connect(self.onMyToolBarButtonClick)
+        pixmapi_save = QStyle.StandardPixmap.SP_DialogSaveButton
+        button_save = QAction(self.style().standardIcon(pixmapi_save), "&Save", self)
+        button_save.setStatusTip("Save current state")
+        button_save.triggered.connect(self.save)
         menu = self.menuBar()
         file_menu = menu.addMenu("&File")
-        file_menu.addAction(button_action)
+        file_menu.addAction(button_folder)
+        file_menu.addAction(button_save)
 
         # Tool Widgets
         frame = QFrame()
@@ -54,13 +60,13 @@ class MainWindow(QMainWindow):
         layout_tools_sub.setDirection(3)
         layout_tools = QHBoxLayout()
         layout_tools.addStretch()
-        cluster_viz = QGridLayout()
+        self.cluster_viz = VisualizeClusterWidget()
 
 
         layout_tools_sub.addWidget(cluster_select)
         layout_tools_sub.addWidget(label_cluster_select)
         layout_tools.addLayout(layout_tools_sub)
-        layout_cluster.addLayout(cluster_viz)
+        layout_cluster.addWidget(self.cluster_viz)
         layout_central.addLayout(layout_cluster)
         layout_central.addLayout(layout_tools)
 
@@ -73,20 +79,18 @@ class MainWindow(QMainWindow):
 
         self.show()
 
-        if os.path.isfile('paths.txt'):
-            if os.path.getsize('paths.txt') != 0:
+        if os.path.isfile('paths.json'):
+            if os.path.getsize('paths.json') != 0:
                 dlg = LoadingDialog()
                 if dlg.exec():
                     self.setWindowTitle("Loading...")
-                    file = open('paths.txt', 'r')
-                    lines = file.readlines()
-                    for fname in lines:
-                        self.data.append(SessionFeature(fname))
+                    with open('data.json', 'w') as f:
+                        json.dump(self.path_list, f)
+                    
+                    for fname, results in self.path_list.values():
+                        self.load_session(fname, results)
 
                     self.setWindowTitle("Cell Clustering Tool")
-        else:
-            with open('paths.txt', 'w') as fp:
-                pass
 
 
 
@@ -132,25 +136,38 @@ class MainWindow(QMainWindow):
         self.threadpool.start(worker)
         '''
         if fname != '' and fname not in self.path_list:
-            self.setWindowTitle("Loading...")
-            events = list(result.keys())
-            events.remove("group")
-            session = SessionFeature(fname, events)
-            for event in events:
-                delay, window = result[event]["delay"], result[event]["window"]
-                session.events[event].set_delay_and_duration(delay, window)
-                session.events[event].set_values()
+            self.load_session(fname, result)
+            
 
-            session.set_group(result["group"])
-            session.set_vector()
-            session.compute_clustering()
-
-            self.setWindowTitle("Cell Clustering Tool")
-
-            self.path_list[fname] = result
+            
         
 
+    def load_session(self, fname, result):
+        self.setWindowTitle("Loading...")
+        events = list(result.keys())
+        events.remove("group")
+        session = SessionFeature(fname, events)
+        for event in events:
+            delay, window = result[event]["delay"], result[event]["window"]
+            session.events[event].set_delay_and_duration(delay, window)
+            session.events[event].set_values()
 
+        session.set_group(result["group"])
+        session.set_vector()
+        session.compute_clustering()
+
+        self.path_list[fname] = result
+        self.sessions.append(session)
+        y = int(session.day[1:])
+        x = 1 if session.session == 'S1' else 2
+        self.cluster_viz.grids[session.group].addVisualization(session.clustering_result, x, y)
+        self.setWindowTitle("Cell Clustering Tool")
+
+    
+    def save(self):
+        if self.path_list:
+            with open('paths.json', 'w') as f:
+                json.dump(self.path_list, f)
 
 
 app = QApplication([])
