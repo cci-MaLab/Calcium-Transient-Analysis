2a461a47a3686583c47aad820d45ea2549f50584
GrowlingM1ke <michallange1995@gmail.com>
2023-11-06 15:58:33 -0500

    Added video tools

--- data_exploration.py
+++ data_exploration.py
@@ -1,10 +1,8 @@
 """
 The following file will be used for doing a deeper dive into the selected session
 """
-from PyQt5.QtWidgets import (QDialog, QDialogButtonBox, QVBoxLayout, QLabel, QLineEdit, QHBoxLayout, QWidget,
-                            QCheckBox, QGridLayout, QFrame, QGraphicsView, QGraphicsScene, QPushButton, 
-                            QComboBox, QListWidget, QAbstractItemView, QSplitter, QApplication, QStyleFactory,
-                            QAction, QFileDialog)
+from PyQt5.QtWidgets import (QVBoxLayout, QHBoxLayout, QWidget, QPushButton, QAction, QStyle, 
+                            QSlider)
 from PyQt5.QtGui import (QIntValidator, QImage, QPixmap, QPainter, QPen, QColor, QBrush, QFont)
 from PyQt5.QtCore import (Qt, QTimer)
 import pyqtgraph as pg
@@ -34,15 +32,7 @@ class ExplorationWidget(QWidget):
                 button_video_type.setChecked(True)
             else:
                 button_video_type.setChecked(False)
-            self.submenu_videos.addAction(button_video_type)
-
-        
-        button_pdf = QAction("&Play", self.imv.getView().menu)
-        button_pdf.triggered.connect(lambda: self.video_timer.start())
-        self.imv.getView().menu.addAction(button_pdf)
-
-        
-                
+            self.submenu_videos.addAction(button_video_type)             
         
 
         # We'll load in a copy of the visualization of the cells we are monitoring
@@ -51,25 +41,89 @@ class ExplorationWidget(QWidget):
         for outlier in self.session.outliers_list:
             self.A.pop(outlier)
 
-        # Layout
-        layout = QHBoxLayout()
-        layout.addWidget(self.imv)
-        self.setLayout(layout)
+        
+        # Tools for video
+        self.pixmapi_play = QStyle.StandardPixmap.SP_MediaPlay
+        self.pixmapi_pause = QStyle.StandardPixmap.SP_MediaPause
+        self.btn_play = QPushButton("")
+        self.btn_play.setCheckable(True)
+        self.btn_play.setIcon(self.style().standardIcon(self.pixmapi_play))
+        self.btn_play.setFixedSize(30, 30)
+        self.btn_play.clicked.connect(self.play_pause)
+        
+        self.pixmapi_forward = QStyle.StandardPixmap.SP_ArrowRight
+        self.btn_forward = QPushButton("")
+        self.btn_forward.setIcon(self.style().standardIcon(self.pixmapi_forward))
+        self.btn_forward.setFixedSize(30, 30)
+        self.btn_forward.clicked.connect(self.clicked_next)
+
+        self.pixmapi_backward = QStyle.StandardPixmap.SP_ArrowLeft
+        self.btn_backward = QPushButton("")
+        self.btn_backward.setIcon(self.style().standardIcon(self.pixmapi_backward))
+        self.btn_backward.setFixedSize(30, 30)
+        self.btn_backward.clicked.connect(self.clicked_prev)
+
+        self.scroll_video = QSlider(Qt.Orientation.Horizontal)
+        self.scroll_video.setRange(0, self.video_length)
+        self.scroll_video.sliderMoved.connect(self.pause_video)
+        self.scroll_video.sliderReleased.connect(self.slider_update)
+        
+
+        # Layouts
+        layout_video_tools = QHBoxLayout()
+        layout_video_tools.addWidget(self.btn_backward)
+        layout_video_tools.addWidget(self.btn_play)
+        layout_video_tools.addWidget(self.btn_forward)
+        layout_video_tools.addWidget(self.scroll_video)
+
+        layout_video = QVBoxLayout()
+        layout_video.addWidget(self.imv)
+        layout_video.addLayout(layout_video_tools)
+
+        self.setLayout(layout_video)
 
         self.video_timer = QTimer()
         self.video_timer.setInterval(50)
         self.video_timer.timeout.connect(self.next_frame)
-        self.video_timer.start()
+
+    def slider_update(self):
+        self.current_frame = self.scroll_video.value() - 1
+        self.next_frame()
+
+    def clicked_next(self):
+        self.pause_video()
+        self.next_frame()
+    
+    def clicked_prev(self):
+        self.pause_video()
+        self.prev_frame()
+
+    def play_pause(self):
+        if self.btn_play.isChecked():
+            self.start_video()
+        else:
+            self.pause_video()            
+
 
     def next_frame(self):
         self.current_frame = (1 + self.current_frame) % self.video_length
+        self.scroll_video.setValue(self.current_frame)
+        self.imv.setImage(self.current_video.sel(frame=self.current_frame).values, autoRange=False) 
+
+    def prev_frame(self):
+        self.current_frame = (self.current_frame - 1) % self.video_length
+        self.scroll_video.setValue(self.current_frame)
         self.imv.setImage(self.current_video.sel(frame=self.current_frame).values, autoRange=False) 
     
     def pause_video(self):
         self.video_timer.stop()
+        self.btn_play.setIcon(self.style().standardIcon(self.pixmapi_play))
+    
+    def start_video(self):
+        self.video_timer.start()
+        self.btn_play.setIcon(self.style().standardIcon(self.pixmapi_pause))
 
     def change_video(self, type):
-        self.pause_video()
         self.current_video = self.videos[type]
         for action in self.submenu_videos.actions():
             if action.text() == f"&{type}":
