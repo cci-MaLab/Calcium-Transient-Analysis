3691b108608b4a82b0c11798c0bda6470425d456
GrowlingM1ke <michallange1995@gmail.com>
2024-01-04 18:57:25 -0500

    Added remove and add event utility

--- data_exploration.py
+++ data_exploration.py
@@ -2,19 +2,22 @@
 The following file will be used for doing a deeper dive into the selected session
 """
 from PyQt5.QtWidgets import (QVBoxLayout, QHBoxLayout, QWidget, QPushButton, QAction, QStyle, 
-                            QSlider, QLabel, QListWidget, QAbstractItemView)
+                            QSlider, QLabel, QListWidget, QAbstractItemView, QLineEdit, QSplitter,
+                            QApplication, QStyleFactory, QFrame)
 from PyQt5.QtCore import (Qt, QTimer)
-from pyqtgraph import PlotItem
+from PyQt5.QtGui import (QIntValidator, QDoubleValidator)
+from pyqtgraph import (PlotItem, PlotCurveItem, ScatterPlotItem)
 import pyqtgraph as pg
 import numpy as np
 from pyqtgraph import InfiniteLine
 from scipy.signal import find_peaks
 
 class ExplorationWidget(QWidget):
-    def __init__(self, session, main_window_ref, parent=None):
+    def __init__(self, session, main_window_ref, timestamps=None, parent=None):
         super().__init__(parent)
         self.session = session
         self.main_window_ref = main_window_ref
+        self.timestamps = timestamps
 
         # Set up main view
         self.imv = pg.ImageView()
@@ -69,6 +72,27 @@ class ExplorationWidget(QWidget):
         self.btn_cell_clear_color.setCheckable(True)
         self.btn_cell_clear_color.clicked.connect(self.refresh_image)
 
+        # Plot utility
+        self.min_height_label = QLabel("Height Threshold")
+        self.min_height_input = QLineEdit()
+        self.min_height_input.setValidator(QDoubleValidator(0, 1000, 3))
+        self.min_height_input.setText("0")
+
+        self.dist_label = QLabel("Min IEI")
+        self.dist_input = QLineEdit()
+        self.dist_input.setValidator(QIntValidator(0, 1000))
+        self.dist_input.setText("10")
+
+        self.auc_label = QLabel("AUC")
+        self.auc_input = QLineEdit()
+        self.auc_input.setValidator(QDoubleValidator(0, 1000, 3))
+        self.auc_input.setText("0")
+
+        btn_algo_event = QPushButton("Calculate Events")
+        btn_algo_event.clicked.connect(self.update_peaks)
+        btn_clear_events = QPushButton("Clear Selected Events")
+        btn_clear_events.clicked.connect(self.clear_selected_events)
+
 
         # Populate cell list
         for id in self.A.keys():
@@ -109,12 +133,11 @@ class ExplorationWidget(QWidget):
 
         # Visualize signals selected in video
         self.w_signals = pg.GraphicsLayoutWidget()
-
         
 
         # Layouts
         layout_video_cells = QHBoxLayout()
-        layout_video_cells_visualize = QVBoxLayout()
+        layout_video_cells_visualize = QHBoxLayout()
 
         layout_video_tools = QHBoxLayout()
         layout_video_tools.addWidget(self.btn_backward)
@@ -133,17 +156,70 @@ class ExplorationWidget(QWidget):
         layout_cells.addWidget(self.btn_cell_reset)
         layout_cells.addWidget(self.btn_cell_clear_color)
 
+        layout_plot_utility = QVBoxLayout()
+        layout_height = QHBoxLayout()
+        layout_height.addWidget(self.min_height_label)
+        layout_height.addWidget(self.min_height_input)
+        layout_plot_utility.addLayout(layout_height)
+        layout_dist = QHBoxLayout()
+        layout_dist.addWidget(self.dist_label)
+        layout_dist.addWidget(self.dist_input)
+        layout_plot_utility.addLayout(layout_dist)
+        layout_auc = QHBoxLayout()
+        layout_auc.addWidget(self.auc_label)
+        layout_auc.addWidget(self.auc_input)
+        layout_plot_utility.addLayout(layout_auc)
+        layout_plot_utility.addWidget(btn_algo_event)
+        layout_plot_utility.addWidget(btn_clear_events)
+        widget_plot_utility = QWidget()
+        widget_plot_utility.setLayout(layout_plot_utility)
+        widget_plot_utility.setMaximumWidth(250)
+
         layout_video_cells.addLayout(layout_video)
         layout_video_cells.addLayout(layout_cells)
+        widget_video_cells = QWidget()
+        widget_video_cells.setLayout(layout_video_cells)
 
-        layout_video_cells_visualize.addLayout(layout_video_cells)
-        layout_video_cells_visualize.addWidget(self.w_signals)
+        layout_video_cells_visualize.addWidget(widget_video_cells)
 
-        self.setLayout(layout_video_cells_visualize)
+        layout_plot = QHBoxLayout()
+        layout_plot.addWidget(self.w_signals)
+        layout_plot.addWidget(widget_plot_utility)
+        widget_plot = QWidget()
+        widget_plot.setLayout(layout_plot)
+        layout_video_cells_visualize.addWidget(widget_plot)
+        widget_video_cells_visualize = QWidget()
+        widget_video_cells_visualize.setLayout(layout_video_cells_visualize)
+
+        layout = QVBoxLayout()
+        main_widget = QSplitter(Qt.Orientation.Vertical)
+        main_widget.setFrameShape(QFrame.StyledPanel)
+        main_widget.setStyleSheet(            
+            "QSplitter::handle{background-color: gray; width: 5px; border: 1px dotted gray}"
+        )
+        main_widget.addWidget(widget_video_cells)
+        main_widget.addWidget(widget_video_cells_visualize)
+        layout.addWidget(main_widget)
+
+        self.setLayout(layout)
+        QApplication.setStyle(QStyleFactory.create('Cleanlooks'))
 
         self.video_timer = QTimer()
         self.video_timer.setInterval(50)
         self.video_timer.timeout.connect(self.next_frame)
+
+    def find_subplot(self, event):
+        if event.double():
+            # We need to identify which Plot Item is under the click
+            items = self.w_signals.scene().items(event.scenePos())
+            plot_item = None
+            for item in items:
+                if isinstance(item, PlotItemEnhanced):
+                    plot_item = item
+                    break
+            if plot_item is not None and isinstance(event.currentItem, PlotCurveItemEnhanced):
+                plot_item.add_point(event)
+        
 
     def highlight_cell(self, event):
         point = self.imv.getImageItem().mapFromScene(event.pos())
@@ -169,13 +245,18 @@ class ExplorationWidget(QWidget):
 
     def visualizeSignals(self, cell_ids):
         self.w_signals.clear()
+        try:
+            self.w_signals.scene().sigMouseClicked.disconnect(self.find_subplot)
+        except:
+            pass
         if cell_ids:
+            self.w_signals.scene().sigMouseClicked.connect(self.find_subplot)
             for i, id in enumerate(cell_ids):
                 # Check if Event data exists
-                p = PlotWidgetEnhanced(id=i)
+                p = PlotItemEnhanced(id=id)
                 self.w_signals.addItem(p, row=i, col=0)
-                data = self.session.data['C'].sel(unit_id=id)
-                p.plot(data)
+                data = self.session.data['C'].sel(unit_id=id).values
+                p.add_main_curve(data)
                 p.plotLine.setPos(self.scroll_video.value())
                 p.setTitle(f"Cell {id}")
                 if 'E' in self.session.data:
@@ -183,8 +264,9 @@ class ExplorationWidget(QWidget):
                     indices = events.nonzero()[0]
                     # Split up the indices into groups
                     indices = np.split(indices, np.where(np.diff(indices) != 1)[0]+1)
-                    for indices_group in indices:
-                        p.plot(indices_group, data[indices_group], pen='r')
+                    # Now Split the indices into pairs of first and last indices
+                    indices = [(indices_group[0], indices_group[-1]+1) for indices_group in indices]
+                    p.draw_event_curves(indices, data)
 
 
 
@@ -276,83 +358,81 @@ class ExplorationWidget(QWidget):
         i = 0
         while self.w_signals.getItem(i,0) is not None:
             item = self.w_signals.getItem(i,0)
-            if isinstance(item, PlotWidgetEnhanced):
+            if isinstance(item, PlotItemEnhanced):
                 item.plotLine.setPos(self.scroll_video.value())
             i += 1
 
     def closeEvent(self, event):
         super(ExplorationWidget, self).closeEvent(event)
         self.main_window_ref.removeWindow(self.name)
-
-    def change_button(self, clicks=None):
-        if self.wgt_flag.button_type == "success":
-            self.wgt_flag.button_type = "danger"
-            self.wgt_flag.name = "Bad Cell"
-            cell_status = False
-        else:
-            self.wgt_flag.button_type = "success"
-            self.wgt_flag.name = "Good Cell"
-            cell_status = True
-        
-        # Update the coordinates of self._C
-        idx, = np.where(self._C["unit_id"].values == self.current_cell)
-        good_cells = self._C["good_cells"].values
-        good_cells[idx] = cell_status
-        self._C = self._C.assign_coords(good_cells=("unit_id", good_cells))
     
-    def update_peaks(self, clicks=None):
-        min_height = self.min_height_input.value
-        distance = self.dist_input.value if self.dist_input.value != 0 else 10
-        auc = self.auc.value
-        if self._normalize:
-            C_signal = self._C_norm.sel(unit_id=self.strm_usub.usub[0]).values
-            S_signal = self._S_norm.sel(unit_id=self.strm_usub.usub[0]).values
-        else:
-            C_signal = self._C_norm_global.sel(unit_id=self.strm_usub.usub[0]).values
-            S_signal = self._S_norm_global.sel(unit_id=self.strm_usub.usub[0]).values
+    def clear_selected_events(self):
+        i = 0
+        while self.w_signals.getItem(i,0) is not None:
+            item = self.w_signals.getItem(i,0)
+            if isinstance(item, PlotItemEnhanced):
+                item.clear_selected_events()
+            i += 1
 
-        peaks, _ = find_peaks(C_signal)
-        self.spikes = []
-        self.peaks = []
-        # We must now determine when the beginning of the spiking occurs. It must satisfy the following conditions:
-        # 1.) Use the C signal to detect all potential peaks.
-        # 2.) Going from left to right start evaluating the distance between peaks. If the next peak is close enough and greater than the current, delete the current peak and allocate the S values to the next peak.
-        # 3.) Check the AUC of all allocated S values of the observed peak. If its less than a threshold then delete it.
-        # 4.) Exclude all peaks whose C value (amplitude) is smaller than a threshold) - I decided to do this last in case we want to allocate the S value to another peak.
-        culminated_s_indices = set() # We make use of a set to avoid duplicates
-        for i, current_peak in enumerate(peaks):
-            # Look at the next peak and see if it is close enough to the current peak
-            peak_height = C_signal[current_peak]
-            # Allocate the overlapping S values to the next peak
-            if S_signal[current_peak] == 0:
-                continue # This indicates no corresponding S signal
-            culminated_s_indices.add(self.get_S_dimensions(S_signal, current_peak))
-            if i < len(peaks) - 1 and C_signal[peaks[i+1]] > peak_height:
-                diff = peaks[i+1] - current_peak if self.timestamps is None else self.timestamps[peaks[i+1]] - self.timestamps[current_peak]
-                if diff <= distance:
-                    continue
+    def update_peaks(self):
+        min_height = int(self.min_height_input.text()) if self.min_height_input.text() else 0
+        distance = float(self.dist_input.text()) if self.dist_input.text() else 10        
+        auc = float(self.auc_input.text()) if self.auc_input.text() else 0
+        
+        i = 0
+        while self.w_signals.getItem(i,0) is not None:
+            item = self.w_signals.getItem(i,0)
+            if isinstance(item, PlotItemEnhanced):
+                C_signal = self.session.data['C'].sel(unit_id=item.id).values
+                S_signal = self.session.data['S'].sel(unit_id=item.id).values
+                peaks, _ = find_peaks(C_signal)
+                spikes = []
+                final_peaks = []
+                # We must now determine when the beginning of the spiking occurs. It must satisfy the following conditions:
+                # 1.) Use the C signal to detect all potential peaks.
+                # 2.) Going from left to right start evaluating the distance between peaks. If the next peak is close enough and greater than the current, delete the current peak and allocate the S values to the next peak.
+                # 3.) Check the AUC of all allocated S values of the observed peak. If its less than a threshold then delete it.
+                # 4.) Exclude all peaks whose C value (amplitude) is smaller than a threshold) - I decided to do this last in case we want to allocate the S value to another peak.
+                culminated_s_indices = set() # We make use of a set to avoid duplicates
+                for i, current_peak in enumerate(peaks):
+                    # Look at the next peak and see if it is close enough to the current peak
+                    peak_height = C_signal[current_peak]
+                    # Allocate the overlapping S values to the next peak
+                    if S_signal[current_peak] == 0:
+                        continue # This indicates no corresponding S signal
+                    culminated_s_indices.add(self.get_S_dimensions(S_signal, current_peak))
+                    if i < len(peaks) - 1 and C_signal[peaks[i+1]] > peak_height:
+                        diff = peaks[i+1] - current_peak if self.timestamps is None else self.timestamps[peaks[i+1]] - self.timestamps[current_peak]
+                        if diff <= distance:
+                            continue
 
-            # Now check the AUC of the current peak we will use the accumulated S values and also keep track of the earliest
-            # index.
-            beg, end = len(S_signal), 0
-            for beg_temp, end_temp in culminated_s_indices:
-                beg = beg_temp if beg_temp < beg else beg
-                end = end_temp if end_temp > end else end
-            
-            culminated_s_indices = set()
+                    # Now check the AUC of the current peak we will use the accumulated S values and also keep track of the earliest
+                    # index.
+                    beg, end = len(S_signal), 0
+                    for beg_temp, end_temp in culminated_s_indices:
+                        beg = beg_temp if beg_temp < beg else beg
+                        end = end_temp if end_temp > end else end
+                    
+                    culminated_s_indices = set()
 
-            if np.sum(S_signal[beg:end]) < auc:
-                continue
+                    if np.sum(S_signal[beg:end]) < auc:
+                        continue
 
-            if C_signal[beg:current_peak+1].max() - C_signal[beg:current_peak+1].min() < min_height:
-                continue
-
-
-            self.spikes.append([beg, current_peak+1])
-            self.peaks.append([current_peak])
+                    if C_signal[beg:current_peak+1].max() - C_signal[beg:current_peak+1].min() < min_height:
+                        continue
 
 
-        self.update_temp_comp_sub()
+                    spikes.append([beg, current_peak+1])
+                    final_peaks.append([current_peak])
+                # Remove events
+                item.clear_event_curves()
+                # Plot spikes
+                item.draw_event_curves(spikes, C_signal)
+                
+                
+            i += 1
+
+
 
 
     def get_S_dimensions(self, S_signal, idx):
@@ -396,11 +476,72 @@ class ExplorationWidget(QWidget):
         return (start, end)
 
 
-class PlotWidgetEnhanced(PlotItem):
-    def __init__(self, id=None, **kwargs):
-        super(PlotWidgetEnhanced, self).__init__(**kwargs)
-        self.id = id
-        self.plotLine = InfiniteLine(pos=0, angle=90, pen='r')
+class PlotItemEnhanced(PlotItem):
+    def __init__(self, **kwargs):
+        super(PlotItemEnhanced, self).__init__(**kwargs)
+        self.id = kwargs["id"] if "id" in kwargs else None
+        self.plotLine = InfiniteLine(pos=0, angle=90, pen='g')
         self.addItem(self.plotLine)
+        self.selected_events = set()
+        self.clicked_points = []
+        
+
+    def clear_event_curves(self):
+        for item in self.listDataItems():
+            if isinstance(item, PlotCurveItemEnhanced):
+                if item.is_event:
+                    self.removeItem(item)
+
+    def draw_event_curves(self, spikes, C_signal):
+        for beg, end in spikes:
+            event_curve = PlotCurveItemEnhanced(np.arange(beg, end), C_signal[beg:end], pen='r', is_event=True, main_plot=self)
+            self.addItem(event_curve)
+
+    def add_main_curve(self, data):
+        main_curve = PlotCurveItemEnhanced(np.arange(len(data)), data, pen='w', is_event=False)
+        self.addItem(main_curve)
+
+    def clear_selected_events(self):
+        for item in self.selected_events:
+            self.removeItem(item)
+        self.selected_events.clear()
+        
+    
+    def add_point(self, event):
+        # Map to this items coordinates
+        point = event.pos()
+        x, y = point.x(), point.y()
+        print(str(x) + ":::" + str(y))
+        # Draw an x at the point
+        if len(self.clicked_points) == 2:
+            for point in self.clicked_points:
+                self.removeItem(point)
+            self.clicked_points = []
+        
+        point = ScatterPlotItem([x], [y], pen='b', symbol='x', size=10)
+        self.addItem(point)
+        self.clicked_points.append(point)
+
+
+
+class PlotCurveItemEnhanced(PlotCurveItem):
+    def __init__(self, *args, **kwargs):
+        super(PlotCurveItemEnhanced, self).__init__(*args, **kwargs)
+        self.is_event = kwargs["is_event"] if "is_event" in kwargs else False
+        self.main_plot = kwargs["main_plot"] if "main_plot" in kwargs else None
+        self.selected = False
+        self.setClickable(True)
+
+        if self.is_event:
+            self.sigClicked.connect(self.clicked)
+    
+    def clicked(self, event):
+        self.selected = not self.selected
+        if self.selected:
+            self.setPen('b')
+            self.main_plot.selected_events.add(self)
+        else:
+            self.setPen('r')
+            self.main_plot.selected_events.remove(self)
 
 
