da120fcb77d1d8457254ce48457a9fb8332ebb40
Haoying Fu <rainbow_pet@163.com>
2024-02-01 07:02:44 -0500

    update

--- core/testbackend.py
+++ core/testbackend.py
@@ -1,22 +1,37 @@
 import sys
 import os
 sys.path.insert(0, ".")
+<<<<<<< Updated upstream:core/testbackend.py
 from core.backend import DataInstance
 from core.backend import Event
+=======
+from backend import DataInstance
+from backend import Event
+from genetic_algorithm import Genetic_Algorithm
+>>>>>>> Stashed changes:testbackend.py
 import matplotlib.pyplot as plt
 
+
+
 def main():
-    events = ['ALP','IALP']
-    newsession = DataInstance("/N/project/Cortical_Calcium_Image/Miniscope data/05.2023_Tenth_group/AA058_D1/2023_05_05/11_02_42/Miniscope_2/S4/config.ini",events )
-    for i in events:
-        newsession.events[i].set_delay_and_duration(0,20)
-        newsession.events[i].set_values()
-    # newsession.outliers_list = [1,2]
-    newsession.set_vector()
-    print(newsession.values)  
-    newsession.compute_clustering()
-    # print(newsession.linkage_data)
-    # plt.imshow(newsession.clustering_result)
+    # events = ['ALP','IALP']
+    ga = Genetic_Algorithm()
+    ga.execute()
+    # preBinNum,postBinNum,binSize,fitness, examples= ga.return_results(0)
+    preBinNum = ga.preBinNum
+    postBinNum = ga.postBinNum
+    binSize = ga.binSize
+    fitness = ga._best_fitness
+    examples = ga.examples
+    frameline = ga.xvalues
+    print('preBinNum: ',preBinNum)
+    print('postBinNum: ' ,postBinNum)
+    print('binSize: ' ,binSize)
+    # print('xvalues',frameline[0])
+    # print('examples:',examples)
+        # print('AUCs: ' ,AUCs[i])
+
+    
 
 if __name__ == "__main__":
     main()
