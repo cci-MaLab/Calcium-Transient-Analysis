ee77a59c8297b98ea1f3be50c011734ef6031fe4
GrowlingM1ke <michallange1995@gmail.com>
2024-04-24 14:25:47 -0400

    Started work on transformer implementation

--- ml_training/dataset.py
+++ ml_training/dataset.py
@@ -297,3 +297,77 @@ class MouseData:
         sample = torch.stack([Yra_sample, C_sample, DFF_sample]).T
 
         return sample, target
+
+class LocalTransformerDataset:
+    def __init__(self, paths_to_unit_ids, section_len=200, rolling=50, slack=50):
+        '''
+        We will do a bit different approach here the size of each sample will be section_len + 2 * slack. We will slide the window by rolling.
+        For the final and first sample we'll pad the input with zeros. We no longer need to keep track of hidden states.
+        '''
+        self.paths_to_unit_ids = paths_to_unit_ids
+        self.data = []
+        
+        for path, unit_ids in paths_to_unit_ids.items():
+            data = open_minian(path)
+            E = data['E'].sel(unit_id=unit_ids)
+            YrA = data['YrA'].sel(unit_id=unit_ids)
+            C = data['C'].sel(unit_id=unit_ids)
+            DFF = data['DFF'].sel(unit_id=unit_ids)
+
+            # Normalize the datasets locally
+            YrA = YrA / YrA.max(dim='frame')
+            C = C / C.max(dim='frame')
+            DFF = DFF / DFF.max(dim='frame')
+
+            # We want the data to be preloaded into memory for faster access.
+            # Convert into float32 tensors
+            YrA = torch.tensor(YrA.values.astype(np.float32)).to(config.DEVICE)
+            C = torch.tensor(C.values.astype(np.float32)).to(config.DEVICE)
+            DFF = torch.tensor(DFF.values.astype(np.float32)).to(config.DEVICE)
+            input_data = torch.stack([YrA, C, DFF]).T
+            E = torch.tensor(E.values.astype(np.float32)).to(config.DEVICE)
+
+            
+    
+
+def train_val_test_split(paths, val_split=0.1, test_split=0.1):
+    total_len = 0
+    path_to_ids = {}
+    for path in paths:
+        data = open_minian(path)
+
+        all_unit_ids = data['E'].unit_id.values
+        verified = data['E'].verified.values.astype(int)
+        unit_ids = all_unit_ids[verified==1]
+        total_len += len(unit_ids)
+        path_to_ids[path] = unit_ids
+    
+    total_val = int(val_split * total_len)
+    total_test = int(test_split * total_len)
+    random_picks = np.random.choice(np.arange(total_len), total_val + total_test, replace=False)
+    val_picks, test_picks = random_picks[:total_val], random_picks[total_val:]
+
+    train_unit_ids = {}
+    val_unit_ids = {}
+    test_unit_ids = {}
+
+    boost = 0
+    for path, unit_ids in path_to_ids.items():
+        for i in range(len(unit_ids)):
+            adjusted_idx = i + boost
+            if adjusted_idx in val_picks:
+                if path not in val_unit_ids:
+                    val_unit_ids[path] = []
+                val_unit_ids[path].append(unit_ids[i])
+            elif adjusted_idx in test_picks:
+                if path not in test_unit_ids:
+                    test_unit_ids[path] = []
+                test_unit_ids[path].append(unit_ids[i])
+            else:
+                if path not in train_unit_ids:
+                    train_unit_ids[path] = []
+                train_unit_ids[path].append(unit_ids[i])
+
+        boost += len(unit_ids)
+
+    return train_unit_ids, val_unit_ids, test_unit_ids
